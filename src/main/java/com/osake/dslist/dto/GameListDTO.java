ef1076b75acf148c176cc43cb10704ff19697441
Heitor-Saqueto <heitorsaqueto@hotmail.com>
2024-11-16 20:24:54 -0300

    Get lists

---
+++ src/main/java/com/osake/dslist/dto/GameListDTO.java
@@ -0,0 +1,26 @@
+package com.osake.dslist.dto;
+
+import com.osake.dslist.entities.GameList;
+
+public class GameListDTO {
+	
+	private Long id;
+	private String name;
+	
+
+	public GameListDTO() {
+	}
+	public GameListDTO(GameList entity) {
+		id = entity.getId();
+		name = entity.getName();
+	}
+	public Long getId() {
+		return id;
+	}
+	public String getName() {
+		return name;
+	}
+	
+	
+
+}
