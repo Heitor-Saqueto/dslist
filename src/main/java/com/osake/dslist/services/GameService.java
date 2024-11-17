1fa6ccaa7d1e356eb31063e0475ae0d40b3e8831
Heitor-Saqueto <heitorsaqueto@hotmail.com>
2024-11-16 21:18:47 -0300

    Search SQL, projection , search game by list

--- src/main/java/com/osake/dslist/services/GameService.java
+++ src/main/java/com/osake/dslist/services/GameService.java
@@ -9,6 +9,7 @@ import org.springframework.transaction.annotation.Transactional;
 import com.osake.dslist.dto.GameDTO;
 import com.osake.dslist.dto.GameMinDTO;
 import com.osake.dslist.entities.Game;
+import com.osake.dslist.projection.GameMinProjection;
 import com.osake.dslist.repositories.GameRepository;
 
 
@@ -27,6 +28,12 @@ public class GameService {
 	@Transactional(readOnly = true)
 	public List<GameMinDTO> findAll(){
 		List<Game> result = gameRepository.findAll();
+		return result.stream().map(x -> new GameMinDTO(x)).toList();
+	}
+	
+	@Transactional(readOnly = true)
+	public List<GameMinDTO> findByList(Long listId){
+		List<GameMinProjection> result = gameRepository.searchByList(listId);
 		return result.stream().map(x -> new GameMinDTO(x)).toList();
 	}
 }
